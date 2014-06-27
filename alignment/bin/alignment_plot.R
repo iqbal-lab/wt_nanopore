8cb34122e07042b25e79c4591ed22a6a4bfcfcc3
Phelim Bradley <wave@phel.im>
2014-06-27 10:29:32 +0100

    alignment R script plots some figures and prints some basic alignment stats to screen

--- alignment/bin/alignment_plot.R
+++ alignment/bin/alignment_plot.R
@@ -6,13 +6,42 @@ options(echo=TRUE) # if you want see commands in output file
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 dirname <- args[0]
-dirname <- "/Net/wombat/dipro/mmm/data/Nanopore/processed_data/Run_1/Minion1/stats/"
+# dirname <- "~/Dropbox/Nanopore/processed_data/Run_1/Minion1/stats/"
+
+printf <- function(...)print(sprintf(...))
+
 coverageData <- read.table(paste(dirname,"template_coverage.dat",sep=""),header=T)
 alignmentData <- read.table(paste(dirname,"template_alignment.dat",sep=""),header=T)
 ## Create output directoy 
-dir.create(file.path(dirname, "img"), showWarnings = FALSE)
+outdir <- file.path(dirname, "img")
+dir.create(outdir, showWarnings = FALSE)
+png(paste(outdir,"position_versus_coverage.png",sep="/"),width=1000,height=800)
+ggplot(coverageData,aes(x=pos,y=cov)) + geom_line() + theme_bw() + xlab("Reference Positions") + ylab("Coverage") + ggtitle("Coverage across reference genome")
+dev.off()
+png(paste(outdir,"histogram_of_read_length.png",sep="/"),width=1000,height=800)
+ggplot(alignmentData,aes(x=len)) + geom_histogram(binwidth=100) + theme_bw() + xlab("Read Length") + ggtitle("Histogram of Read Lengths")
+dev.off()
+## Plot length versus alignment length
+png(paste(outdir,"read_length_vs_aligned_read_length.png",sep="/"),width=1000,height=1000)
+ggplot(alignmentData,aes(x=len,y=aligned_length)) + geom_point() + theme_bw() + xlab("Read Length") + ylab("Aligned Read Length") + ggtitle("")
+dev.off()
 
-ggplot(coverageData,aes(x=pos,y=cov)) + geom_line()
-ggplot(alignmentData,aes(x=length)) + geom_histogram()
+numBases <- sum(as.numeric(alignmentData$len))
+numAlignedBases <- sum(as.numeric(alignmentData$aligned_length))
+M <- sum(as.numeric(alignmentData$M ))
+I <- sum(as.numeric(alignmentData$I ))
+D <- sum(as.numeric(alignmentData$D ))
+S <- sum(as.numeric(alignmentData$S ))
+H <- sum(as.numeric(alignmentData$H ))
+longestRead <- max(alignmentData$len)
+longestAlignedRead <- max(alignmentData$aligned_length)
 
-
+printf("Number of Bases: %i", numBases)
+printf("Number of Aligned Bases : %s", numAlignedBases)
+printf("Number of MatchMissMatch Bases : %a", M)
+printf("Number of Inserted Bases: %i", I)
+printf("Number of Deleted Bases: %i", D)
+printf("Number of Softclipped Bases: %i", S)
+printf("Number of HardClipped Bases: %i", H)
+printf("Longest Read: %i", longestRead)
+printf("Longest Aligned Read : %s", longestAlignedRead)
