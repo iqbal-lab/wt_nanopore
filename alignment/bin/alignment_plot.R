4b7f848937af57a9aa5ffc0ae169c0b4776ac362
phelimb <wave@phel.im>
2014-06-27 10:07:50 +0100

    a

--- alignment/bin/alignment_plot.R
+++ alignment/bin/alignment_plot.R
@@ -1,25 +1,18 @@
 #! /usr/bin/env Rscript
 ## Script to take the output of alignment_stats and plot coverage across the reference
-install.packages('ggplot2')
+# install.packages('ggplot2')
 library(ggplot2)
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+dirname <- args[0]
+dirname <- "/Net/wombat/dipro/mmm/data/Nanopore/processed_data/Run_1/Minion1/stats/"
+coverageData <- read.table(paste(dirname,"template_coverage.dat",sep=""),header=T)
+alignmentData <- read.table(paste(dirname,"template_alignment.dat",sep=""),header=T)
+## Create output directoy 
+dir.create(file.path(dirname, "img"), showWarnings = FALSE)
 
-data <- read.table("~/Dropbox/Nanopore/processed_data/Burn_in/m1/stats/m1_lambda_burn_in_coverage.dat",header=T)
-data <- read.table("~/Dropbox/Nanopore/processed_data/Burn_in/m2/stats/m2_lambda_burn_in_coverage.dat",header=T)
-data <- read.table("~/Dropbox/Nanopore/processed_data/Burn_in/m3/stats/m3_lambda_burn_in_coverage.dat",header=T)
+ggplot(coverageData,aes(x=pos,y=cov)) + geom_line()
+ggplot(alignmentData,aes(x=length)) + geom_histogram()
 
-data <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion1/stats/template_coverage.dat",header=T)
-data <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion2/stats/template_coverage.dat",header=T)
-data <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion3/stats/template_coverage.dat",header=T)
-
-ggplot(data,aes(x=pos,y=cov)) + geom_line()
-
-
-data1 <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion1/stats/template_raw_read_stats.dat",header=T)
-data2 <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion2/stats/template_raw_read_stats.dat",header=T)
-data3 <- read.table("~/Dropbox/Nanopore/processed_data/Run_1/Minion3/stats/template_raw_read_stats.dat",header=T)
-
-
-ggplot(data1,aes(x=length)) + geom_histogram()
-ggplot(data2,aes(x=length)) + geom_histogram()
-ggplot(data3,aes(x=length)) + geom_histogram()
 
